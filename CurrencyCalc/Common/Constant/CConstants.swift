db71959d6ac86a149e1d33aaa9eaac870456cd02
luthfifr <luthfir272@gmail.com>
2020-02-14 02:22:40 +0700

    add CConstants file

---
+++ CurrencyCalc/Common/Constant/CConstants.swift
@@ -0,0 +1,15 @@
+//
+//  CConstants.swift
+//  CurrencyCalc
+//
+//  Created by Luthfi Fathur Rahman on 14/02/20.
+//  Copyright © 2020 StandAlone. All rights reserved.
+//
+
+import Foundation
+
+struct CConstants {
+    struct API {
+        static let accessKey = "f22339d3c9a04da3652d945a52f048f0"
+    }
+}
