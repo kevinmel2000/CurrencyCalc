a7e132b2b7c83e7765f240881b6bd648b638dcf4
luthfifr <luthfir272@gmail.com>
2020-02-14 00:17:07 +0700

    add UI elements to input amount and show the converted value of that amount

--- CurrencyCalc/Views/CCMainViewController.swift
+++ CurrencyCalc/Views/CCMainViewController.swift
@@ -16,8 +16,16 @@ class CCMainViewController: UIViewController {
 
     private let disposeBag = DisposeBag()
     
-    private var textField: UITextField!
-    private var currencyMenu: UIPickerView!
+    private var currencySourceLabel: UILabel!
+    private var amountTextField: UITextField!
+    private var upperStackView: UIStackView!
+    private var currencyPicker: UIPickerView!
+    private var currencyListTextField: UITextField!
+    private var convertedAmountLabel: UILabel!
+    private var lowerStackView: UIStackView!
+
+    //dummy
+    private let currencyArr = Observable.just(["AUD", "CNY", "JPY", "SGD"])
 
     // MARK: - Initialization
     convenience init() {
@@ -36,6 +44,8 @@ class CCMainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
 
         setupEvents()
         setupViews()
@@ -56,16 +66,85 @@ extension CCMainViewController {
     }
 
     private func setupViews() {
-        textField = UITextField(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100))
-        textField.backgroundColor = .white
-        textField.keyboardType = .numberPad
-        view.addSubview(textField)
+        setupSourceField()
+        setupConvertedField()
+    }
 
-        textField.snp.makeConstraints({ make in
-            make.leading.trailing.top.equalToSuperview()
-            make.height.equalTo(50)
+    private func setupSourceField() {
+        upperStackView = UIStackView(frame: .zero)
+        upperStackView.distribution = .fill
+
+        currencySourceLabel = UILabel(frame: .zero)
+        currencySourceLabel.text = "USD"
+        currencySourceLabel.font = UIFont.systemFont(ofSize: 32)
+        currencySourceLabel.textAlignment = .left
+        currencySourceLabel.textColor = .black
+        currencySourceLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        upperStackView.addArrangedSubview(currencySourceLabel)
+        
+        amountTextField = UITextField(frame: .zero)
+        amountTextField.backgroundColor = .lightGray
+        amountTextField.keyboardType = .numberPad
+        amountTextField.borderStyle = .bezel
+        amountTextField.textAlignment = .right
+        amountTextField.placeholder = "Type number here"
+        amountTextField.clearsOnBeginEditing = false
+        amountTextField.font = .systemFont(ofSize: 32)
+        amountTextField.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        upperStackView.addArrangedSubview(amountTextField)
+        
+        view.addSubview(upperStackView)
+
+        upperStackView.snp.makeConstraints({ make in
+            make.leading.equalTo(view.snp.leadingMargin)
+            make.trailing.equalTo(view.snp.trailingMargin)
+            make.top.equalTo(view.snp.topMargin).offset(25)
         })
     }
+
+    private func setupConvertedField() {
+        lowerStackView = UIStackView(frame: .zero)
+        lowerStackView.distribution = .fill
+
+        currencyPicker = UIPickerView()
+        currencyArr.bind(to: currencyPicker.rx.itemTitles) { _, element in
+            return element
+        }.disposed(by: disposeBag)
+
+        currencyPicker.rx.itemSelected.subscribe(onNext: { [weak self] row, value in
+            guard let `self` = self else { return }
+            self.currencyArr.elementAt(value).subscribe(onNext: { elementValue in
+                self.currencyListTextField.text = elementValue[row]
+            }).disposed(by: self.disposeBag)
+        }).disposed(by: disposeBag)
+
+        currencyListTextField = UITextField(frame: .zero)
+        currencyListTextField.backgroundColor = .lightGray
+        currencyListTextField.borderStyle = .bezel
+        currencyListTextField.textAlignment = .left
+        currencyListTextField.placeholder = "--Choose--"
+        currencyListTextField.font = .systemFont(ofSize: 32)
+        currencyListTextField.inputView = currencyPicker
+        currencyListTextField.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        lowerStackView.addArrangedSubview(currencyListTextField)
+        
+        convertedAmountLabel = UILabel(frame: .zero)
+        convertedAmountLabel.text = "0"
+        convertedAmountLabel.font = UIFont.systemFont(ofSize: 32)
+        convertedAmountLabel.textAlignment = .right
+        convertedAmountLabel.textColor = .black
+        convertedAmountLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        lowerStackView.addArrangedSubview(convertedAmountLabel)
+
+        view.addSubview(lowerStackView)
+
+        lowerStackView.snp.makeConstraints({ make in
+            make.leading.equalTo(currencySourceLabel.snp.leading)
+            make.trailing.equalTo(amountTextField.snp.trailing)
+            make.top.equalTo(amountTextField.snp.bottom).offset(25)
+        })
+    }
+    
 
     @objc private func dismissKeyboard() {
         view.endEditing(true)
