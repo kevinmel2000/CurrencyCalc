fafefcb3d1f86649d000baae785ac4e516ca149d
luthfifr <luthfir272@gmail.com>
2020-02-13 22:16:51 +0700

    add codes to initiate rootVC, CCMainViewController and CCMainViewModel

---
+++ CurrencyCalc/ViewModels/CCMainViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  CCMainViewModel.swift
+//  CurrencyCalc
+//
+//  Created by Luthfi Fathur Rahman on 13/02/20.
+//  Copyright © 2020 StandAlone. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxOptional
+
+enum CCMainViewModelEvent: Equatable {
+    case getCurrenciesData
+
+    static func == (lhs: CCMainViewModelEvent, rhs: CCMainViewModelEvent) -> Bool {
+        switch (lhs, rhs) {
+        case (getCurrenciesData, getCurrenciesData):
+            return true
+        }
+    }
+}
+
+protocol CCMainViewModelViewModelType {
+    var uiEvents: PublishSubject<CCMainViewModelEvent> { get }
+    var viewModelEvents: PublishSubject<CCMainViewModelEvent> { get }
+}
+
+final class CCMainViewModel: CCMainViewModelViewModelType {
+    let uiEvents = PublishSubject<CCMainViewModelEvent>()
+    let viewModelEvents = PublishSubject<CCMainViewModelEvent>()
+    let disposeBag = DisposeBag()
+}
+
+// MARK: - Private Methods
+extension CCMainViewModel {
+    private func setupEvents() {
+        viewModelEvents.subscribe(onNext: { [weak self] event in
+            guard let `self` = self else { return }
+            switch event {
+            case .getCurrenciesData:
+                self.getCurrenciesData()
+            }
+        }).disposed(by: disposeBag)
+    }
+
+    private func getCurrenciesData() {
+        
+    }
+}
---
+++ CurrencyCalc/Views/CCMainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CCMainViewController.swift
+//  CurrencyCalc
+//
+//  Created by Luthfi Fathur Rahman on 13/02/20.
+//  Copyright © 2020 StandAlone. All rights reserved.
+//
+
+import UIKit
+
+class CCMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
