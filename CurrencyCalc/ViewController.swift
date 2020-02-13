81f6f241cfccce1757ce13106f2e9323cc334b31
luthfifr <luthfir272@gmail.com>
2020-02-13 21:29:52 +0700

    Initial Commit

---
+++ CurrencyCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyCalc
+//
+//  Created by Luthfi Fathur Rahman on 13/02/20.
+//  Copyright © 2020 StandAlone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
